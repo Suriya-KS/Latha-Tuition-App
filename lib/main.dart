ad8e53c4a745cc7645a5923b50c64bc194a3fa25
Suriya <work.suriyaks@gmail.com>
2024-03-09 14:01:45 +0530

    chore: :wrench: setup & initialize firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'package:latha_tuition_app/firebase_options.dart';
 import 'package:latha_tuition_app/utilities/app_theme.dart';
 import 'package:latha_tuition_app/screens/tutor_dashboard.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
